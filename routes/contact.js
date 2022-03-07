8328c433159ffde5e692efad12da50b7af466750
wajdi.benhajyahya@esprit.tn <wajdi.benhajyahya@esprit.tn>
2022-03-07 19:51:23 +0100

    Search specefic Contact

--- routes/contact.js
+++ routes/contact.js
@@ -5,8 +5,8 @@ var Contact = require('../model/contact');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     Contact.find(function (err, data) {
-        if (err) throw  err;
-        res.render('getAllContacts.twig', {data});
+        if (err) throw err;
+        res.render('getAllContacts.twig', { data });
         console.log(data);
     });
 });
@@ -28,8 +28,8 @@ router.post('/addAction', function (req, res, next) {
 });
 router.get('/delete/:id', function (req, res, next) {
     var id = req.params.id;
-    Contact.findOneAndRemove({_id: id}, (err) => {
-        if (err) throw  err;
+    Contact.findOneAndRemove({ _id: id }, (err) => {
+        if (err) throw err;
     })
     res.redirect('/contact');
 });
@@ -37,22 +37,50 @@ router.get('/delete/:id', function (req, res, next) {
 
 router.get('/modifier/:id', function (req, res, next) {
     var id = req.params.id;
-    Contact.findById({_id: id}, (err, data) => {
-        if (err) throw  err;
-        res.render('addContact.twig', {data});
+    Contact.findById({ _id: id }, (err, data) => {
+        if (err) throw err;
+        res.render('addContact.twig', { data });
         console.log(data);
     });
 });
 router.post('/update/:id', function (req, res, next) {
     var id = req.params.id;
-    Contact.findByIdAndUpdate({_id: id}, {
-            FullName: req.body.fullname,
-            Phone: req.body.phone
-        },
+    Contact.findByIdAndUpdate({ _id: id }, {
+        FullName: req.body.fullname,
+        Phone: req.body.phone
+    },
         (err) => {
             if (err) throw err;
         });
     res.redirect('/contact');
 });
 
+router.post('/search', function (req, res, next) {
+    console.log("tkalem" + req.body.Search)
+    Contact.find(function (err, data1) {
+        if (err) throw err;
+        let data = [];
+        if (req.body.Search == "") {
+            res.redirect('/contact');
+        } else {
+            data1.forEach(element => {
+                if (req.body.Search == element.FullName) {
+
+                    data.push(element)
+                    console.log("dataa est : " + data)
+                }
+
+            });
+        }
+        if (data == null) {
+            res.render('getAllContacts.twig', { data1 });
+        } else {
+            res.render('getAllContacts.twig', { data });
+        }
+        //     res.render('getAllContacts.twig', {data});
+        //     console.log(data);
+
+        // });
+    });
+});
 module.exports = router;
